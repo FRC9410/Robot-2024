3bac1209098585ba39a81e103f8e8880b89cb891
Chad <chad.lee@live.com>
2024-02-17 00:17:24 -0600

    Oopsie

--- src/main/java/frc/robot/commands/group/ScoreAmpCommand.java
+++ src/main/java/frc/robot/commands/group/ScoreAmpCommand.java
@@ -29,12 +29,12 @@ public class ScoreAmpCommand extends SequentialCommandGroup {
         new IntakeCommand(subsystems.getIntake(), 30, 12, 0)
       ),
       new ParallelRaceGroup(
-        new ShooterWristCommand(subsystems.getShooter(), 16, false),
+        new ShooterWristCommand(subsystems.getShooter(), 16, true),
         new WaitCommand(1)
       ),
       new ParallelRaceGroup(
         new WaitCommand(1),
-        new ShooterWristCommand(subsystems.getShooter(), 16, true),
+        new ShooterWristCommand(subsystems.getShooter(), 16, false),
         new ShootCommand(subsystems.getShooter(), 30),
         new VoltageFeedCommand(subsystems.getShooter(), -30)
       )
