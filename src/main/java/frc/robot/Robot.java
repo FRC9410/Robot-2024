136581d288c5fabfa3afb2727968b1db92f810c8
Chad <chad.lee@live.com>
2024-03-06 19:48:57 -0600

    Removing Line of Code to Eliminate Unpredictability

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -41,8 +41,6 @@ public class Robot extends TimedRobot {
     else {
       robotContainer.getSubsystems().getVision().setPipeline(VisionType.SHOOTER, 2);
     }
-
-    robotContainer.getSubsystems().getDrivetrain().pointWheels(0);
   }
 
   @Override
